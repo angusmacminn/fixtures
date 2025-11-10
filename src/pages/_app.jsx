f075e5c55c7b9aef436506073ee152b9507b3144
Angus MacMinn <angus13@hotmail.ca>
2025-11-10 15:03:34 -0800

    added in helvetica font using next/font

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,41 @@
 import "@/styles/globals.scss";
+import localFont from 'next/font/local'
+
+const helvetica = localFont({
+  src: [
+    {
+      path: '../fonts/helvetica-light-587ebe5a59211.woff',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Helvetica.woff',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Helvetica-Oblique.woff',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../fonts/Helvetica-Bold.woff',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../fonts/Helvetica-BoldOblique.woff',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-helvetica',
+})
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={helvetica.className}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
